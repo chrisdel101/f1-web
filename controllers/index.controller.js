be4bc9c17dffa267aeb4f2cdfbf1138ca6d1b5ae
Chris Del <drumgod101@gmail.com>
2019-07-11 18:33:43 -0400

    get json working in controller

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -1,7 +1,24 @@
 const https = require('https')
 const title = 'Formula 1'
+const json = require('../test.json')
 module.exports = {
-  getDriver: id => {},
+  render: async (ctx, next) => {
+    ctx.body = await module.exports.getDriver()
+    let test = await module.exports.getDriver()
+    console.log(test.stage)
+    // await ctx.render('index', {})
+  },
+  getDriver: async id => {
+    // const stageUrl = `https://api.sportradar.us/formula1/trial/v2/en/sport_events/sr:stage:324771/summary.json?api_key=${
+    //   process.env.F1
+    // }`
+    // const call = module.exports.httpCall(stageUrl)
+    // const json = await call
+    // return call
+    return new Promise((resolve, reject) => {
+      resolve(json)
+    })
+  },
   httpCall: url => {
     return new Promise((resolve, reject) => {
       https.get(url, res => {
@@ -10,11 +27,6 @@ module.exports = {
           resolve(d)
         })
       })
-    })
-  },
-  render: async (ctx, next) => {
-    await ctx.render('index', {
-      title: title
     })
   }
 }
