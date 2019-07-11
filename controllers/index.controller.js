3ac8025d2549d1db1c8c4f011b1a855ae0aa739a
Chris Del <drumgod101@gmail.com>
2019-07-11 19:21:47 -0400

    get form and driver displaying

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -3,23 +3,29 @@ const title = 'Formula 1'
 const json = require('../test.json')
 module.exports = {
   render: async (ctx, next) => {
-    ctx.body = await module.exports.getDriver()
-    let test = await module.exports.getDriver()
-    console.log(test.stage)
-    // await ctx.render('index', {})
-  },
-  getDriver: async id => {
-    // const stageUrl = `https://api.sportradar.us/formula1/trial/v2/en/sport_events/sr:stage:324771/summary.json?api_key=${
-    //   process.env.F1
-    // }`
-    // const call = module.exports.httpCall(stageUrl)
-    // const json = await call
-    // return call
-    return new Promise((resolve, reject) => {
-      resolve(json)
+    let drivers = await module.exports.getDrivers()
+    await ctx.render('index', {
+      title: title,
+      drivers: drivers,
+      method: 'POST',
+      action: '/driver',
+      enctype: 'application/x-www-form-urlencoded',
+      buttonField: 'Submit'
     })
   },
-  httpCall: url => {
+  getDrivers: async id => {
+    const stageUrl = `https://api.sportradar.us/formula1/trial/v2/en/sport_events/sr:stage:324771/summary.json?api_key=${
+      process.env.F1
+    }`
+    const call = module.exports.httpCall(stageUrl)
+    let json = await call
+    json = JSON.parse(json)
+    const names = json.stage.competitors.map(obj => {
+      return obj.name
+    })
+    return names
+  },
+  httpCall: async url => {
     return new Promise((resolve, reject) => {
       https.get(url, res => {
         res.setEncoding('utf8')
