5ab79d2a15bbbcdc69f60f1486a905669c255f83
Chris Del <drumgod101@gmail.com>
2019-07-23 18:44:35 -0400

    move drivers call to own obj

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -1,12 +1,42 @@
 const utils = require('../utils')
 module.exports = {
   render: async (ctx, next) => {
-    let drivers;
+    const driversObj = await module.exports.handleDrivers(ctx, next)
+    // console.log('drivers', driversObj)
+    const teamsObj = await module.exports.handleTeams(ctx, next)
+    await ctx.render('index', {
+      title: ctx.title,
+      method: 'POST',
+      action: '/driver',
+      enctype: 'application/x-www-form-urlencoded',
+      buttonField: 'Submit',
+      buttonType: 'submit',
+      buttonValue: 'submit',
+      driverSelectName: driversObj.selectName,
+      driverEnums: driversObj.driversArr
+    })
+  },
+  handleDrivers: async (ctx, next) => {
     try {
-      drivers = JSON.parse(await utils.fetchData('drivers'))
-
-      await ctx.render('index', {
-        title: ctx.title,
+      const driversArr = JSON.parse(await utils.fetchData('drivers'))
+      return {
+        driversArr: driversArr,
+        selectName: 'selectDriver'
+      }
+    } catch {
+      const error = {
+        message: 'Internal Server Error',
+        status: 500,
+        detail: 'Could not parse driver information.',
+        stack: new Error().stack
+      }
+      await ctx.render('error', error)
+    }
+  },
+  handleTeams: async (ctx, next) => {
+    try {
+      const teams = JSON.parse(await utils.fetchData('teams'))
+      return {
         enums: drivers,
         method: 'POST',
         action: '/driver',
@@ -16,15 +46,15 @@ module.exports = {
         buttonValue: 'submit',
         selectName: 'selectDriver',
         routeName: 'index'
-      })
+      }
     } catch {
       const error = {
-        message: "Internal Server Error",
+        message: 'Internal Server Error',
         status: 500,
-        detail: "Could not parse driver information.",
-        stack: (new Error()).stack
-      };
-      await ctx.render('error', error);
+        detail: 'Could not parse driver information.',
+        stack: new Error().stack
+      }
+      await ctx.render('error', error)
     }
   }
 }
