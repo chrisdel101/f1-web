0c9918cf9f509d5010bb873d5c83013510ade6c0
Chris Del <drumgod101@gmail.com>
2019-08-24 11:16:52 -0600

    add driver flags. Get team styles done

--- controllers/teams.controller.js
+++ controllers/teams.controller.js
@@ -16,10 +16,20 @@ async function fetchTeam(ctx, next) {
   // get data from form
   let teamName = ctx.query.team
   const formData = await handleFormData()
+  // list of drivers
   const driversObj = formData.drivers
+  // list of teams
   const teamsObj = formData.teams
   const teamData = JSON.parse(await utils.fetchData(`teams/${teamName}`))
-  console.log('TD', teamData)
+  const driver1Slug = teamData.drivers_scraped[0].name_slug
+  const driver2Slug = teamData.drivers_scraped[1].name_slug
+  // get driver flags from api
+  const driverFlags = [
+    JSON.parse(await utils.fetchData(`drivers/${driver1Slug}`))['flag_img_url'],
+    JSON.parse(await utils.fetchData(`drivers/${driver2Slug}`))['flag_img_url']
+  ]
+  teamData['driverFlags'] = driverFlags
+  console.log('flags', driverFlags)
   await ctx.render('team', {
     urls: ctx.urls,
     title: ctx.title,
@@ -37,7 +47,8 @@ async function fetchTeam(ctx, next) {
     teamSelectName: teamsObj.selectName,
     driverAction: driversObj.driverAction,
     driverSelectName: driversObj.selectName,
-    teamData: teamData
+    teamData: teamData,
+    driverFlags: driverFlags
   })
 }
 module.exports = {
