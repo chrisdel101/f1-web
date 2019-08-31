e832e111141bfeff06853490b61dbe8e727fcbe4
Chris Del <drumgod101@gmail.com>
2019-08-31 14:04:18 -0600

    get file serving and working with drivers tested

--- API/index.js
+++ API/index.js
@@ -1,16 +1,19 @@
 const utils = require('../utils')
 const send = require('koa-send')
 const fs = require('fs')
+
 async function sendImage(ctx) {
   // take image
   await utils.takeImage(ctx)
-  console.log('CTX', ctx.res)
+  //   set content type to image
   ctx.type = `image/png`
-  ctx.body = fs.createReadStream(
-    '/Users/chrisdielschnieder/desktop/code_work/formula1/f1Web/example.png'
-  )
-  // grab image
-  //   return ctx
+  //   send image to body
+  ctx.body = fs.createReadStream('./example.png')
+  //   unlink/delete file
+  fs.unlink('./example.png', err => {
+    if (err) throw err
+    console.log('path/file.txt was deleted')
+  })
 }
 module.exports = {
   sendImage
