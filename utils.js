5b7c6afb9e0e616fe8fb4a0ca14e34471a42f9e7
Chris Del <drumgod101@gmail.com>
2019-08-31 17:37:28 -0600

    add try/catch

--- utils.js
+++ utils.js
@@ -104,18 +104,25 @@ module.exports = {
     }
   },
   takeImage: async ctx => {
-    const browser = await puppeteer.launch({
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
-    })
-    const page = await browser.newPage()
-    if (process.env.NODE_ENV === 'development') {
-      await page.goto(`http://localhost:3000/driver/${ctx.params.driver_slug}`)
-    } else if (process.env.NODE_ENV === 'production') {
-      await page.goto(
-        `https://f1-cards.herokuapp.com/api/driver/${ctx.params.driver_slug}`
-      )
+    try {
+      const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+      })
+      const page = await browser.newPage()
+      if (process.env.NODE_ENV === 'development') {
+        await page.goto(
+          `http://localhost:3000/driver/${ctx.params.driver_slug}`
+        )
+      } else if (process.env.NODE_ENV === 'production') {
+        await page.goto(
+          `https://f1-cards.herokuapp.com/api/driver/${ctx.params.driver_slug}`
+        )
+      }
+      await page.screenshot({ path: 'example.png' })
+      await browser.close()
+    } catch (e) {
+      console.error('An error occured in takeImage:', e)
+      return 'An error occured in takeImage:', e
     }
-    await page.screenshot({ path: 'example.png' })
-    await browser.close()
   }
 }
