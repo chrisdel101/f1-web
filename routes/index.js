7169bc49d3b3d6eb1c2b08873ddc8b8698fcab19
Chris Del <drumgod101@gmail.com>
2019-09-02 09:46:42 -0600

    get examples inside route

--- routes/index.js
+++ routes/index.js
@@ -44,4 +44,43 @@ router.get('/test', async ctx => {
     console.error('An error in take image', e)
   }
 })
+router.get('/:driver_slug', async ctx => {
+  try {
+    if (fs.existsSync('./example.png')) {
+      fs.unlinkSync('./example.png')
+      console.log('Old Image was removed')
+    } else {
+      console.log('Old Image not removed')
+    }
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    })
+    const page = await browser.newPage()
+    if (process.env.NODE_ENV === 'development') {
+      console.log('here')
+      await page.goto(`http://localhost:3000/driver/${ctx.params.driver_slug}`)
+    } else if (process.env.NODE_ENV === 'production') {
+      await page.goto(
+        `https://f1-cards.herokuapp.com/api/driver/${ctx.params.driver_slug}`
+      )
+    }
+    await page.screenshot({ path: 'example.png' })
+
+    await browser.close()
+    try {
+      if (fs.existsSync('./example.png')) {
+        console.log('Image was taken')
+        ctx.type = `image/png`
+        //   send image to body
+        ctx.body = fs.createReadStream('./example.png')
+      } else {
+        console.log('Image not taken')
+      }
+    } catch (err) {
+      console.error(err)
+    }
+  } catch (e) {
+    console.error('An error in take image', e)
+  }
+})
 module.exports = router
