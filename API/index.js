a7ff562b8cb5a53fb636edda866bd254ca170b88
Chris Del <drumgod101@gmail.com>
2019-09-02 09:34:09 -0600

    move into indexAPI

--- API/index.js
+++ API/index.js
@@ -1,10 +1,30 @@
 const utils = require('../utils')
-const send = require('koa-send')
 const fs = require('fs')
+var puppeteer = require('puppeteer')
 
 async function sendImage(ctx) {
-  // take image
-  await utils.takeImage(ctx)
+  try {
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    })
+    console.log('HERE 1')
+    const page = await browser.newPage()
+    if (process.env.NODE_ENV === 'development') {
+      await page.goto(`http://localhost:3000/driver/${ctx.params.driver_slug}`)
+    } else if (process.env.NODE_ENV === 'production') {
+      await page.goto(
+        `https://f1-cards.herokuapp.com/api/driver/${ctx.params.driver_slug}`
+      )
+      console.log('HERE 2')
+    }
+    await page.screenshot({ path: 'example.png' })
+    console.log('here 3')
+    await browser.close()
+    console.log('here 3')
+  } catch (e) {
+    console.error('An error occured in takeImage:', e)
+    return 'An error occured in takeImage:', e
+  }
   //   set content type to image
   ctx.type = `image/png`
   //   send image to body
