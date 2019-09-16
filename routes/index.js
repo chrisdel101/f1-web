c22330b4292da6fc31d16a575943bce88b333910
Chris Del <drumgod101@gmail.com>
2019-09-15 20:16:58 -0600

    add comments

--- routes/index.js
+++ routes/index.js
@@ -8,16 +8,19 @@ var puppeteer = require('puppeteer')
 
 router.get('/', indexController.render)
 router.get('/reset-cache', indexController.resetCache)
+// render full template
 router.get('/driver', driversController.renderDriverTemplate)
 router.get('/team', teamsController.renderTeamTemplate)
+// render just cards
 router.get('/driver/:driver_slug', driversController.renderDriverCard)
 router.get('/team/:team_slug', teamsController.renderTeamCard)
-router.get('/testapi/team/:team_slug', teamsController.fetchTeamAPI)
+// send image to messenger
 router.get('/api/driver/:driver_slug', async ctx => {
   return API.sendImage(ctx, 'driver').then(res => {
     return (ctx.body = res)
   })
 })
+// / send image to messenger
 router.get('/api/team/:team_slug', async ctx => {
   return API.sendImage(ctx, 'team').then(res => {
     return (ctx.body = res)
