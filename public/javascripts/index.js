8b5659a31831d0223e14e132fac6254df6489971
Chris Del <drumgod101@gmail.com>
2019-11-14 23:37:02 -0600

    get index of each element on page with indexof

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,20 +1,51 @@
-const driverCards = document.querySelectorAll(".driver-card.mini a")
+let driverCardsLinks = document.querySelectorAll(".driver-card.mini a")
+driverCardsLinks = Array.from(driverCardsLinks)
+// let driverCards = document.querySelectorAll(".driver-card.mini")
 let driverObjs = []
 let lastChecked
-driverCards.forEach(driverCardElem => {
+driverCardsLinks.forEach(driverCardElem => {
   driverCardElem.addEventListener("click", function(e) {
     e.preventDefault()
-    const driverObj = new createDriverObj(e)
-    driverObjs.push(driverObj)
-    lastChecked = driverObjs[driverObjs.length - 1]
+    const pageIndex = driverCardsLinks.indexOf(driverCardElem)
+    const driverObj = new createDriverObj(e, pageIndex)
+    const exists = driverObjs.some(obj => obj.name_slug === driverObj.name_slug)
+    // check new obj, to avoid doubles
+    if (!exists) {
+      console.log("does not exists: push", exists)
+      driverObjs.push(driverObj)
+    }
     // add class to parent li elem
-    driverObj.element.parentNode.classList.add("selected")
     driverObj.clicked = true
-    //   Going downwards
-    // if second check is greater than first
-    if (driverObjs[0] < lastChecked)
-      if (e.shiftKey) {
-        console.log("two")
+    // add class to current
+    toggleClickedClass(driverObj)
+    // note last checked
+    lastChecked = driverObjs[driverObjs.length - 1]
+    console.log("last", lastChecked)
+    console.log("last index", driverObjs.indexOf(lastChecked))
+
+    if (!e.shiftKey) {
+      console.log("two", driverObjs.indexOf(lastChecked))
+      for (let i = 0; i < driverObjs.indexOf(lastChecked); i++) {
+        driverObjs[i].clicked = false
+        toggleClickedClass(driverObjs[i])
+        // console.log("all up to", driverObjs[i])
+        // toggleClickedClass(driverObjs[i])
+      }
+      // // slice off all arr - move current to start
+      driverObjs.splice(0, driverObjs.length - 1)
+      console.log("arr", driverObjs)
+    }
+
+    // // console.log("lastChecked", lastChecked)
+    // // add class to parent li elem
+    // // driverObj.element.parentNode.classList.add("selected")
+    // // driverObj.clicked = true
+    // //   Going downwards
+    // // if second check is greater than first
+    // /
+    if (e.shiftKey) {
+      if (driversObj < driverlastChecked) {
+        console.log("one")
         // start at first index of array of indexs - check from start to current
         for (let i = driverObjs[0]; i < lastChecked; i++) {
           // fill in all boxes
@@ -22,44 +53,20 @@ driverCards.forEach(driverCardElem => {
           driverObjs[i].element.classList.add("selected")
         }
       }
-    if (!e.shiftKey) {
-      console.log("two")
-      // add class to parent li elem
-      driverObj.clicked = true
-      toggleClickedClass(driverObj)
-      console.log("lastChecked", lastChecked)
-      // uncheck all up to but not current
-      for (let i = 0; i < driverObjs.indexOf(lastChecked); i++) {
-        driverObjs[i].clicked = false
-        console.log(driverObjs[i])
-        toggleClickedClass(driverObjs[i])
-      }
-      // move current to start of array
-      driverObjs.splice(0, driverObjs.length - 1)
     }
-    //   going upwards
-    if (driverObjs[0] > lastChecked)
-      if (e.shiftKey) {
-        console.log("three")
-        // start at first index of array of indexs
-        // for (var i = lastChecked; i < driverObjs[0]; i++) {
-        //   // fill in all boxes
-        //   inputs[i].checked = true
-        // }
-      }
+    // going upwards
+    // if (driverObjs[0] > lastChecked) {
+    //   if (e.shiftKey) {
+    //     console.log("three")
+    //     // start at first index of array of indexs
+    //     // for (var i = lastChecked; i < driverObjs[0]; i++) {
+    //     //   // fill in all boxes
+    //     //   inputs[i].checked = true
+    //     // }
+    //   }
+    // }
   })
 })
-
-// function checkAndShift(obj) {}
-// function checkAndUnshift(obj) {
-//   // console.log(e.shiftKey)
-//   //   if (e.target.checked === false && e.shiftKey) {
-//   //     return true
-//   //   } else {
-//   //     return false
-//   //   }
-// }
-function sliceAndLastClicked(arr) {}
 function toggleClickedClass(driverObj) {
   if (driverObj.clicked) {
     driverObj.element.parentNode.classList.add("selected")
@@ -67,54 +74,11 @@ function toggleClickedClass(driverObj) {
     driverObj.element.parentNode.classList.remove("selected")
   }
 }
-function createDriverObj(e) {
+function createDriverObj(e, pageIndex) {
+  this.pageIndex = pageIndex
   this.name_slug = e.target.parentNode.parentNode.dataset.slug
   this.name = e.target.childNodes[0].childNodes[1].innerText
   this.shiftKey = e.shiftKey
   this.element = e.target
   this.clicked = false
 }
-
-// // get array of inputs
-// var inputs = document.querySelectorAll("input")
-// //  make nodelist and array
-// inputs = Array.from(inputs)
-// //  add listner to seach box
-// inputs.forEach(input => {
-//   input.addEventListener("click", e => {
-//     var index = inputs.indexOf(e.target)
-//     console.log(index)
-//     checkedOrder.push(index)
-//     lastChecked = checkedOrder[checkedOrder.length - 1]
-//     // console.log(x)xx ;
-
-//     // Going downwards
-//     // if second check is greater than first
-//     if (checkedOrder[0] < lastChecked)
-//       if (checkAndShift(e) === true) {
-//         console.log(index)
-//         // start at first index of array of indexs
-//         for (var i = checkedOrder[0]; i < lastChecked; i++) {
-//           // fill in all boxes
-//           inputs[i].checked = true
-//         }
-//       }
-//     if (checkAndUnshift(e) === true) {
-//       console.log(index)
-//       for (var i = checkedOrder[0]; i < lastChecked; i++) {
-//         inputs[i].checked = false
-//       }
-//     }
-//     //   going upwards
-//     if (checkedOrder[0] > lastChecked)
-//       if (checkAndShift(e) === true) {
-//         console.log(index)
-//         // start at first index of array of indexs
-//         for (var i = lastChecked; i < checkedOrder[0]; i++) {
-//           // fill in all boxes
-//           inputs[i].checked = true
-//         }
-//       }
-//   })
-// })
-// // get e.childNodes[0].childNodes[0] innerHTML driver name
