322915094a1e03a9624753d0df825dc9810d6c06
Chris Del <drumgod101@gmail.com>
2019-11-15 18:36:57 -0600

    parts of select working. Needs reconfigure

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,56 +1,63 @@
 let driverCardsLinks = document.querySelectorAll(".driver-card.mini a")
 driverCardsLinks = Array.from(driverCardsLinks)
-// let driverCards = document.querySelectorAll(".driver-card.mini")
+// array of objs on the page currently selected
 let driverObjs = []
 let lastChecked
 driverCardsLinks.forEach(driverCardElem => {
   driverCardElem.addEventListener("click", function(e) {
     e.preventDefault()
+    // index of current within the html nodelist on physical page
     const pageIndex = driverCardsLinks.indexOf(driverCardElem)
     const driverObj = new createDriverObj(e, pageIndex)
+    // check if current obj is already in objs array
     const exists = driverObjs.some(obj => obj.name_slug === driverObj.name_slug)
     // check new obj, to avoid doubles
     if (!exists) {
-      console.log("does not exists: push", exists)
+      console.log("does not exists: push if false:", exists)
       driverObjs.push(driverObj)
     }
+    // index of current within the elements selected
+    const arrIndex = driverObjs.indexOf(driverObj)
+    console.log("arrIndex", arrIndex)
+
     // add class to parent li elem
     driverObj.clicked = true
     // add class to current
     toggleClickedClass(driverObj)
     // note last checked
-    lastChecked = driverObjs[driverObjs.length - 1]
-    console.log("last", lastChecked)
-    console.log("last index", driverObjs.indexOf(lastChecked))
+    // lastChecked = driverObjs[driverObjs.length - 1]
+    // console.log("current Arr", driverObjs)
+    // console.log("last checked top", lastChecked)
 
     if (!e.shiftKey) {
-      console.log("two", driverObjs.indexOf(lastChecked))
-      for (let i = 0; i < driverObjs.indexOf(lastChecked); i++) {
+      console.log("====two", driverObjs.indexOf(lastChecked))
+      // uncheck all objs in array up to not including current
+      for (let i = 0; i < arrIndex; i++) {
+        console.log(i)
         driverObjs[i].clicked = false
         toggleClickedClass(driverObjs[i])
-        // console.log("all up to", driverObjs[i])
-        // toggleClickedClass(driverObjs[i])
       }
-      // // slice off all arr - move current to start
+      // // slice off all arr - move current obj to start
       driverObjs.splice(0, driverObjs.length - 1)
-      console.log("arr", driverObjs)
+      // console.log("arr", driverObjs)
     }
 
     // // console.log("lastChecked", lastChecked)
     // // add class to parent li elem
     // // driverObj.element.parentNode.classList.add("selected")
     // // driverObj.clicked = true
-    // //   Going downwards
-    // // if second check is greater than first
     // /
     if (e.shiftKey) {
-      if (driversObj < driverlastChecked) {
+      // //   Going downwards
+      // // if second pageindex is greater than last one
+      if (driverObj.pageIndex > lastChecked.pageIndex) {
         console.log("one")
-        // start at first index of array of indexs - check from start to current
-        for (let i = driverObjs[0]; i < lastChecked; i++) {
+        // start at first index of obj array  - check all from start to current
+        console.log("current obj index", arrIndex)
+        for (let i = driverObjs[0]; i < arrIndex; i++) {
           // fill in all boxes
-          console.log(driverObjs[i].element.classList.add)
-          driverObjs[i].element.classList.add("selected")
+          driverObjs[i].clicked = true
+          toggleClickedClass(driverObjs[i])
         }
       }
     }
@@ -65,12 +72,17 @@ driverCardsLinks.forEach(driverCardElem => {
     //     // }
     //   }
     // }
+    lastChecked = driverObjs[driverObjs.length - 1]
+    console.log("last check bottom", lastChecked)
+    console.log("last index", driverObjs.indexOf(lastChecked))
   })
 })
 function toggleClickedClass(driverObj) {
   if (driverObj.clicked) {
+    console.log("toggle on", driverObj)
     driverObj.element.parentNode.classList.add("selected")
   } else {
+    console.log("toggle off", driverObj)
     driverObj.element.parentNode.classList.remove("selected")
   }
 }
