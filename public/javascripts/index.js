33aa8519706e73e5e56f334eac8fb99ad15bcdb4
Chris Del <drumgod101@gmail.com>
2019-11-17 13:08:22 -0600

    get post req working - push to server

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -24,19 +24,6 @@ driverSubmitButton.addEventListener("click", async () => {
   try {
     const data = returnClickedCardsSlugs()
     return await postData("/drivers", data)
-    // fetch("/drivers", {
-    //   method: "POST", // *GET, POST, PUT, DELETE, etc.
-    //   mode: "cors", // no-cors, *cors, same-origin
-    //   cache: "default", // *default, no-cache, reload, force-cache, only-if-cached
-    //   credentials: "same-origin", // include, *same-origin, omit
-    //   headers: {
-    //     "Content-Type": "application/json"
-    //     // 'Content-Type': 'application/x-www-form-urlencoded',
-    //   },
-    //   redirect: "follow", // manual, *follow, error
-    //   referrer: "no-referrer", // no-referrer, *client
-    //   body: JSON.stringify({ test: "43" }) // body data type must match "Content-Type" header
-    // })
   } catch (e) {
     console.error("An error in submitting all drivers occured", e)
   }
