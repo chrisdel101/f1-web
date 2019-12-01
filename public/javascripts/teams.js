a2ab2f663731a3dc31fc9ef54b840a6500ce2c29
Chris Del <drumgod101@gmail.com>
2019-12-01 15:11:51 -0600

    update teams to also work

--- public/javascripts/teams.js
+++ public/javascripts/teams.js
@@ -1,78 +1,82 @@
-let teamCardLinks = document.querySelectorAll(".team-card.mini a")
+let teamCardLinks = document.querySelectorAll('.team-card.mini a')
 
 teamCardLinks = Array.from(teamCardLinks)
 // array of objs on the page currently selected
 let teamObjs = []
 let lastChecked
+let context
 teamCardLinks.forEach((teamCardElem, i) => {
   const teamObj = new createTeamObj(teamCardElem, i)
   teamObjs.push(teamObj)
-  teamCardElem.addEventListener("click", function(e) {
+  teamCardElem.addEventListener('click', function(e) {
     e.preventDefault()
     if (!is_touch_device()) {
-      console.log("not touch", is_touch_device())
+      console.log('not touch', is_touch_device())
       keyboardCardSelect(teamCardElem, e)
     } else if (is_touch_device()) {
-      console.log("touch", is_touch_device())
-      // touchCardSelect(teamCardElem, e)
+      console.log('touch', is_touch_device())
+      touchCardSelect(teamCardElem, e)
     } else {
-      throw new TypeError("Browser type - touch or non-touch - not detected.")
+      throw new TypeError('Browser type - touch or non-touch - not detected.')
     }
   })
 })
 
-const teamSubmitButton = document.querySelector("button.submit-all-teams")
+const teamSubmitButton = document.querySelector('button.submit-all-teams')
 if (teamSubmitButton) {
-  teamSubmitButton.addEventListener("click", async () => {
+  teamSubmitButton.addEventListener('click', async () => {
     try {
-      const data = await returnClickedCardsSlugs()
-      return await postData("/teams", data)
+      const data = {
+        context,
+        teamsArr: await returnClickedCardsSlugs()
+      }
+      console.log(data)
+      return await postData('/teams', data)
     } catch (e) {
-      console.error("An error in submitting all drivers occured", e)
+      console.error('An error in submitting all teams occured', e)
     }
   })
 }
 
 async function postData(url, data) {
-  if (!data.length || !data) return
-  console.log("click submit")
+  if (isObjEmpty(data)) return 'No data'
+  console.log('click submit')
   const response = await fetch(url, {
-    method: "POST", // *GET, POST, PUT, DELETE, etc.
-    mode: "cors", // no-cors, *cors, same-origin
-    cache: "default", // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: "same-origin", // include, *same-origin, omit
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'default', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
     headers: {
-      "Content-Type": "application/json"
-      // 'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Type': 'application/json'
     },
-    redirect: "follow", // manual, *follow, error
-    referrer: "no-referrer", // no-referrer, *client
+    redirect: 'follow', // manual, *follow, error
+    referrer: 'no-referrer', // no-referrer, *client
     body: JSON.stringify(data) // body data type must match "Content-Type" header
   })
-  return await response.json() // parses JSON response into native JavaScript objects
+  return await response // parses JSON response into native JavaScript objects
 }
 async function returnClickedCardsSlugs() {
   try {
     return teamObjs.filter(team => team.clicked).map(team => team.name_slug)
   } catch (e) {
-    console.error("An error occured while gathering all selected cards", e)
+    console.error('An error occured while gathering all selected cards', e)
   }
 }
 // toggles current card on/off - touch only
 function touchCardSelect(teamCardElem, e) {
   const nodeNameSlug = teamCardElem.parentNode.parentNode.dataset.slug
-  const currentDriverObj = driverObjs.filter(driver => {
-    if (driver.name_slug === nodeNameSlug) {
-      return driver
+  const currentTeamObj = teamObjs.filter(team => {
+    if (team.name_slug === nodeNameSlug) {
+      return team
     }
   })[0]
   // select current driver
-  if (!currentDriverObj.clicked) {
-    currentDriverObj.clicked = true
+  if (!currentTeamObj.clicked) {
+    currentTeamObj.clicked = true
   } else {
-    currentDriverObj.clicked = false
+    currentTeamObj.clicked = false
   }
-  toggleClickedClass(currentDriverObj)
+  toggleClickedClass(currentTeamObj)
 }
 
 // toggles current card with shift bar action
@@ -84,12 +88,11 @@ function keyboardCardSelect(teamCardElem, e) {
       return team
     }
   })[0]
-  // select current driver
+  // select current team
   currentTeamObj.clicked = true
   toggleClickedClass(currentTeamObj)
   // un-click all previous
   if (!e.shiftKey) {
-    // console.log("====two")
     // make current clicked true
     if (lastChecked) {
       // forward
@@ -125,7 +128,6 @@ function keyboardCardSelect(teamCardElem, e) {
           i <= currentTeamObj.pageIndex;
           i++
         ) {
-          // console.log("current obj index", i)
           // fill in all boxes
           teamObjs[i].clicked = true
           toggleClickedClass(teamObjs[i])
@@ -136,7 +138,6 @@ function keyboardCardSelect(teamCardElem, e) {
           i >= currentTeamObj.pageIndex;
           i--
         ) {
-          // console.log("current obj index", i)
           // fill in all boxes
           teamObjs[i].clicked = true
           toggleClickedClass(teamObjs[i])
@@ -148,16 +149,16 @@ function keyboardCardSelect(teamCardElem, e) {
   lastChecked = currentTeamObj
   // console.log("last checked assigned", lastChecked)
 }
-function toggleClickedClass(driverObj) {
-  // console.log(driverObj)
-  if (driverObj.clicked) {
-    console.log("toggle on", driverObj.name)
-    // driverObj.clicked = true
-    driverObj.originalElement.parentNode.classList.add("selected")
+function toggleClickedClass(teamObj) {
+  // console.log(teamObj)
+  if (teamObj.clicked) {
+    console.log('toggle on', teamObj.name)
+    // teamObj.clicked = true
+    teamObj.originalElement.parentNode.classList.add('selected')
   } else {
-    console.log("toggle off", driverObj.name)
-    // driverObj.clicked = false
-    driverObj.originalElement.parentNode.classList.remove("selected")
+    console.log('toggle off', teamObj.name)
+    // teamObj.clicked = false
+    teamObj.originalElement.parentNode.classList.remove('selected')
   }
 }
 function createTeamObj(elem, pageIndex) {
@@ -170,13 +171,14 @@ function createTeamObj(elem, pageIndex) {
 }
 // https://stackoverflow.com/a/4819886/5972531
 function is_touch_device() {
-  var prefixes = " -webkit- -moz- -o- -ms- ".split(" ")
+  var prefixes = ' -webkit- -moz- -o- -ms- '.split(' ')
   var mq = function(query) {
     return window.matchMedia(query).matches
   }
 
   if (
-    "ontouchstart" in window ||
+    'ontouchstart' in window ||
+    // eslint-disable-next-line no-undef
     (window.DocumentTouch && document instanceof DocumentTouch)
   ) {
     return true
@@ -184,6 +186,37 @@ function is_touch_device() {
 
   // include the 'heartz' as a way to have a non matching MQ to help terminate the join
   // https://git.io/vznFH
-  var query = ["(", prefixes.join("touch-enabled),("), "heartz", ")"].join("")
+  var query = ['(', prefixes.join('touch-enabled),('), 'heartz', ')'].join('')
   return mq(query)
+}
+async function getContext() {
+  return new Promise((resolve, reject) => {
+    // eslint-disable-next-line no-undef
+    MessengerExtensions.getContext(
+      '428256864460216',
+      function success(thread_context) {
+        resolve(thread_context)
+      },
+      function error(err) {
+        if (err) {
+          console.error('An error occured in the getContext()', err)
+          resolve(err)
+        }
+      }
+    )
+  })
+}
+function isObjEmpty(obj) {
+  if (Object.keys(obj).length === 0 && obj.constructor === Object) {
+    return true
+  }
+  return false
+}
+window.extAsyncInit = function() {
+  // eslint-disable-next-line no-undef
+  getContext().then(res => {
+    // set context to global scope
+    context = res
+    console.log('contex ready', context)
+  })
 }
